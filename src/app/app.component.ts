66f86f67cf030a76a65924a13f6b8330bddbf82c
Andrei <bagiac.andrei@yahoo.ro>
2019-12-15 18:31:58 +0200

    my first commit with Angular8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
+
+const PATH_LOCATION = "../assets/images/";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'third-app';
+  name = 'third-app';
+  items = [];
+
+  constructor() {
+    this.formItems()
+  }
+
+  formItems() {
+    for (var i = 1; i < 5; i++) {
+      var imgName = require("../assets/images/picture1.png");
+      this.items.push(imgName);
+    }
+  };
+
 }
